399cb9a91e8af80d47daa483e4fdc22805525673
bernmp-dev <bernardomp.dev@gmail.com>
2024-07-16 14:19:32 -0300

    Initial commit

---
+++ javer-persistence-service/src/main/java/com/bernmpdev/javerpersistenceservice/JaverPersistenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bernmpdev.javerpersistenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaverPersistenceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaverPersistenceServiceApplication.class, args);
+    }
+
+}
---
+++ javer-persistence-service/src/test/java/com/bernmpdev/javerpersistenceservice/JaverPersistenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernmpdev.javerpersistenceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaverPersistenceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
