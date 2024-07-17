8c648828508540470bf7817e65dade7e693011d1
bernmp-dev <bernardomp.dev@gmail.com>
2024-07-17 14:04:39 -0300

    feat: javer proxy service application initial config commit

---
+++ javer-proxy-service/src/main/java/com/bernmpdev/javerproxyservice/JaverProxyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bernmpdev.javerproxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaverProxyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaverProxyServiceApplication.class, args);
+    }
+
+}
---
+++ javer-proxy-service/src/test/java/com/bernmpdev/javerproxyservice/JaverProxyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernmpdev.javerproxyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaverProxyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
