3785afd466d17342b7ec83821b7e4c1c5a82f046
bernmp-dev <bernardomp.dev@gmail.com>
2024-07-29 18:35:09 -0300

    feat: javer-gateway-service;

---
+++ javer-gateway-service/src/test/java/com/bernmpdev/javergatewayservice/JaverGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernmpdev.javergatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaverGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
