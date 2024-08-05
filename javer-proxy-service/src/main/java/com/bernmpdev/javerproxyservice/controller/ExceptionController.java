41c5ba921541f857496fa4eac4bb53277ed45df8
bernmp-dev <bernardomp.dev@gmail.com>
2024-08-05 14:32:02 -0300

    refactor: handleFeignException;

--- javer-proxy-service/src/main/java/com/bernmpdev/javerproxyservice/controller/ExceptionController.java
+++ javer-proxy-service/src/main/java/com/bernmpdev/javerproxyservice/controller/ExceptionController.java
@@ -5,36 +5,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 @ControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(FeignException.class)
-    public ResponseEntity<ResponseBuilder> handleFeignException(FeignException ex) {
+    public ResponseEntity<String> handleFeignException(FeignException ex) {
 
         HttpStatus status = HttpStatus.resolve(ex.status());
-        List<String> messages = new ArrayList<>();
-        String errorMessage = ex.contentUTF8();
 
         if (status == null) status = HttpStatus.INTERNAL_SERVER_ERROR;
 
-        if (status == HttpStatus.BAD_REQUEST && errorMessage.startsWith("{")) {
-            messages = parseErrorMessage(errorMessage);
-        } else {
-            messages.add(errorMessage);
-        }
-
         return ResponseEntity
                 .status(status)
-                .body(new ResponseBuilder(status, messages)
-                );
+                .body(ex.contentUTF8());
     }
 
     @ExceptionHandler(Exception.class)
@@ -51,16 +37,4 @@ public class ExceptionController {
     }
 
     public record ResponseBuilder (HttpStatus status, List<String> messages) {}
-
-    private List<String> parseErrorMessage(String errorMessage) {
-        List<String> messages = new ArrayList<>();
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            Map<String, String> errors = objectMapper.readValue(errorMessage, HashMap.class);
-            errors.forEach((element, message) -> messages.add(message));
-        } catch (IOException e) {
-            messages.add(errorMessage);
-        }
-        return messages;
-    }
 }
